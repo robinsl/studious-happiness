99d7a99c5fd81111813fbe2b8b2b886cbcd3d9d0
Robin Simard Lamarre <robinlamarre@gmail.com>
2023-10-08 18:52:55 -0400

    Add theme and telescope

---
+++ lua/user/plugins.lua
@@ -0,0 +1,50 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then 
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim.git",
+    install_path
+  }
+
+  print "Installing Packer... Close and reopen NeoVim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup
+]]
+
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return packer.startup(function(use)
+  use "wbthomason/packer.nvim" 
+  use "nvim-lua/popup.nvim" 
+  use "nvim-lua/plenary.nvim"
+  --use { "catppuccin/nvim", as = "catppuccin" } 
+  use "folke/tokyonight.nvim"
+  use "nvim-telescope/telescope.nvim"
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
