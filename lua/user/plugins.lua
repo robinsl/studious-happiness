7a2a7db71b1e030f40aa7b714a73101055a1ad9f
Robin Simard Lamarre <robinlamarre@gmail.com>
2023-10-11 18:29:36 -0400

    lazy setup init

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,71 +1,42 @@
 local fn = vim.fn
 
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local lazypath = fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim.git",
-    install_path
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  "nvim-lua/popup.nvim",
+  "nvim-lua/plenary.nvim",
+  "hrsh7th/nvim-cmp",
+
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lua",
+  "saadparwaiz1/cmp_luasnip",
+
+  'L3MON4D3/LuaSnip',
+  'rafamadriz/friendly-snippets',
+
+  "folke/tokyonight.nvim",
+  "nvim-telescope/telescope.nvim",
+
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  'jose-elias-alvarez/null-ls.nvim',
+
+  "nvim-treesitter/nvim-treesitter",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
   }
+})
 
-  print "Installing Packer... Close and reopen NeoVim..."
-  vim.cmd [[packadd packer.nvim]]
-end
-
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup
-]]
-
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
-
-return packer.startup(function(use)
-  use "wbthomason/packer.nvim"
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
-
-  -- cmp
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-  use "saadparwaiz1/cmp_luasnip"
-
-  -- snippet
-  use 'L3MON4D3/LuaSnip'
-  use 'rafamadriz/friendly-snippets'
-
-  --use { "catppuccin/nvim", as = "catppuccin" } 
-  use "folke/tokyonight.nvim"
-  use "nvim-telescope/telescope.nvim"
-
-  use "neovim/nvim-lspconfig"
-  use "williamboman/mason.nvim" -- simple to use language server installer
-  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
-  use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
-
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
