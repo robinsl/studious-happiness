f2c62396185e60cde405bc9dd61e8ae25e571084
Robin Simard Lamarre <robinlamarre@gmail.com>
2023-10-12 12:13:50 -0400

    add neogit

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -37,4 +37,15 @@ require("lazy").setup({
 
   "startup-nvim/startup.nvim",
   "nvim-lualine/lualine.nvim",
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",        -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true
+  }
 })
